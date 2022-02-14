2180391c363d2c550c10e031b32acf3245e58753
Handy <innocenthandy@hotmail.fr>
2022-02-14 16:10:04 +0100

    frontend en développement

---
+++ frontend/vuefrontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>the end</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <menu none/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import menu from './components/menu.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    menu
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/vuefrontend/src/components/menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="menu">
+    <nav>
+        <h1>logo groupomania</h1>
+        <ul>
+        <li><a href=#>Accueil</a></li>
+        <li><a href=#>A propos</a></li>
+        <li><a href=#>Cpntact</a></li>
+        <li><a href=#>idée 1</a></li>
+        <li><a href=#>idée 2</a></li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
