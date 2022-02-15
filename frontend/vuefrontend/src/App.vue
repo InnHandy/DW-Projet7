2e1ce8a5b24de4adbbf02e384a5144d925402a12
Handy <innocenthandy@hotmail.fr>
2022-02-15 17:23:07 +0100

    code frontend et router

--- frontend/vuefrontend/src/App.vue
+++ frontend/vuefrontend/src/App.vue
@@ -1,33 +1,24 @@
 <template>
-  <div id="app">
-    <h1>the end</h1>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <menu none/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import menu from './components/menu.vue'
-
-
 export default {
-  name: 'App',
+  data() {
+    return {
+      user: null,
+    };
+  },
   components: {
-    HelloWorld,
-    menu
-  }
-}
+    
+  },
+  methods: {
+   
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
 </style>
+
